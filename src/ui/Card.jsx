bdc7f624e3008f4e21ba032bbc1cbd35f756f296
Sreehari P V <sreehari.valsan@calpinetech.com>
2024-05-20 16:42:39 +0530

    Updated css and ReadMe

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -13,7 +13,7 @@ function Card() {
   return (
     <div className="bg-primary w-[90%] md:w-[70%] max-w-[480px] rounded-xl flex flex-col   aspect-[9/16] md:aspect-[9/14] py-5">
       <Info />
-      <section className=" flex flex-col grow px-10 pt-5 gap-5">
+      <section className=" flex flex-col grow px-5 md:px-10 pt-5 pb-[2rem] md:pb-0 gap-5">
         {links.map((link, index) => (
           <LinkButton to={link.url} key={index}>
             {link.name}
